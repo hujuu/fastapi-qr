1ce5c3d81bf95601b143309debb0e62df974c874
hujuu <1961946+hujuu@users.noreply.github.com>
2025-05-27 13:38:57 +0900

    add httpx

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import asyncio
+
+import httpx
 import requests
 import json
 from fastapi import FastAPI, HTTPException
 from typing import Optional
-from data import get_user, User, get_books_by_category
-
+from data import get_user, User, get_books_by_category, Book
+from book_schemas import BookSchema, BookResponseSchema
 app = FastAPI()
+
+async def fetch_address(zipcode: str):
+    """Fetch address information based on zipcode."""
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}",
+            params={"zipcode": zipcode}
+        )
+        return response.json()
+
+
+books: list[BookResponseSchema] = [
+    BookResponseSchema(id=1, title="1984", category="Fiction"),
+    BookResponseSchema(id=2, title="To Kill a Mockingbird", category="Fiction"),
+    BookResponseSchema(id=3, title="The Great Gatsby", category="Fiction"),
+    BookResponseSchema(id=4, title="A Brief History of Time", category="Science"),
+    BookResponseSchema(id=5, title="The Selfish Gene", category="Science")
+]
 
 @app.get("/")
 async def root():
@@ -24,6 +45,25 @@ async def read_books(category: Optional[str] = None) -> list[dict[str, str]]:
     books = get_books_by_category(category)
     return [{"id": book.id, "title": book.title, "category": book.category} for book in books]
 
+
+@app.post("/books/", response_model=BookResponseSchema)
+def create_book(book: BookSchema):
+    new_book_id = max([book.id for book in books], default=0) + 1
+    new_book = BookResponseSchema(id=new_book_id, **book.model_dump())
+    books.append(new_book)
+    return new_book
+
+
+@app.get("/book/", response_model=list[BookResponseSchema])
+def read_book():
+    return books
+
+
+@app.get("/address/")
+async def read_address():
+    zipcodes = ["1060044", "1000001", "1500001"]
+    return await asyncio.gather(*(fetch_address(zipcode) for zipcode in zipcodes))
+
 url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=1060044"
 
 zip = "1060044"
@@ -33,10 +73,6 @@ param = {"zipcode": zip}
 res = requests.get(url, params=param)
 
 data = json.loads(res.text)
-
-print(data)
-
-print('*' * 50)
 
 if data['results'] is not None:
     address_info = data['results'][0]
